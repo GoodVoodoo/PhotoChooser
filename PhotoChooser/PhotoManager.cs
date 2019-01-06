46807656f6b3a7354546b82a6b1ac9f02cf3fe75
IvanD <duluich@gmail.com>
2019-01-07 00:10:15 +0300

    First commit. Basic Functionality

---
+++ PhotoChooser/PhotoManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PhotoChooser
+{
+    class PhotoManager
+    {
+        public List<string> ChoosenPhotoFileNames { get; set; } = new List<string>();
+
+        string startDirectoryPath;
+        public string StartDirectoryPath
+        {
+            get { return startDirectoryPath; }
+            set
+            {
+                startDirectoryPath = value;
+                FillChoosenPhotosList();
+            }
+        }
+        public string OutputDirectoryPath { get; set; }
+
+        void FillChoosenPhotosList()
+        {
+            ChoosenPhotoFileNames.Clear();
+
+            if (string.IsNullOrEmpty(StartDirectoryPath))
+                return;
+
+            var photos = Directory.EnumerateFiles(startDirectoryPath, "*.jpg*");
+
+            foreach (var photo in photos)
+                ChoosenPhotoFileNames.Add(Path.GetFileName(photo));
+        }
+
+        public BitmapImage GetPhoto(string photoName)
+        {
+            BitmapImage photo = new BitmapImage();
+            photo.BeginInit();
+            var pathToPhotoFile = Path.Combine(StartDirectoryPath, photoName);
+            photo.UriSource = new Uri(pathToPhotoFile);
+            photo.EndInit();
+            return photo;
+        }
+    }
+}
---
+++ PhotoChooser/ViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace PhotoChooser
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public string StartDirectoryName { get; set; }
+        public string OutputDirectoryName { get; set; }
+
+        string initialPhotoDirectoryName;
+        public string InitialPhotoDirectoryName
+        {
+            get { return initialPhotoDirectoryName; }
+            set {
+                initialPhotoDirectoryName = value;
+                photoManager.StartDirectoryPath = initialPhotoDirectoryName;
+                FillInitilaPhotosListFromModel();
+            }
+        }
+        public BitmapImage CurrentPhoto { get; set; }
+        public string CurrentPhotoName { get; set; }
+        public ObservableCollection<string> InintilalPhotosList { get; set; } = new ObservableCollection<string>();
+        public ICommand OpenInitialPhotoDirectoryCommand { get; set; }
+        public ICommand OpenNextPhotoCommand { get; set; }
+        public ICommand OpenPreviousPhotoCommand { get; set; }
+
+        readonly PhotoManager photoManager = new PhotoManager();
+
+        public ViewModel()
+        {
+            OpenInitialPhotoDirectoryCommand = new RelayCommand(p => true, a => OpenInitialPhotoDirectory());
+            OpenNextPhotoCommand = new RelayCommand(p => true, a => ShowNextPhoto());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OpenInitialPhotoDirectory()
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    InitialPhotoDirectoryName = folderBrowserDialog.SelectedPath;
+                    OnPropertyChanged(nameof(InitialPhotoDirectoryName));
+                }
+            }
+        }
+
+        void FillInitilaPhotosListFromModel()
+        {
+            foreach (var photo in photoManager.ChoosenPhotoFileNames)
+                InintilalPhotosList.Add(photo);
+
+            CurrentPhotoName = InintilalPhotosList.First();
+            OnPropertyChanged(CurrentPhotoName);
+            CurrentPhoto = photoManager.GetPhoto(CurrentPhotoName);
+            OnPropertyChanged(nameof(CurrentPhoto));
+        }
+
+        void ShowNextPhoto()
+        {
+            string nextPhotoName = InintilalPhotosList[InintilalPhotosList.IndexOf(CurrentPhotoName) + 1];
+            CurrentPhotoName = nextPhotoName;
+            OnPropertyChanged(CurrentPhotoName);
+            CurrentPhoto = photoManager.GetPhoto(CurrentPhotoName);
+            OnPropertyChanged(nameof(CurrentPhoto));
+        }
+
+        void ShowPreviousPhoto()
+        {
+
+        }
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
